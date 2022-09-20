438d0d3d4b1a4ff6e29ebb29483321b3d57af882
CHAITANYA9999999 <98775345+CHAITANYA9999999@users.noreply.github.com>
2022-09-20 21:07:40 +0530

    Create java_end_of_file.java

---
+++ JAVA/java_end_of_file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int i=1;
+        while(s.hasNext()){
+            System.out.print(i+" ");
+            String ss = s.nextLine();
+            System.out.println(ss);
+            i+=1;
+        }
+    }
+}
