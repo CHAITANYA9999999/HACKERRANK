e82f754a2408232ca4d60d9f370ae44cbbcde1c3
CHAITANYA9999999 <98775345+CHAITANYA9999999@users.noreply.github.com>
2022-09-20 21:05:49 +0530

    Create java_loops_I.java

---
+++ JAVA/java_loops_I.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
